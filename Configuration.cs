e8ef1e5c589e4da9b585a3c4b9046d2380e26064
Lincoml Rufino <linconldasilvarufino@gmail.com>
2022-08-07 20:29:43 -0300

    Finished

--- Configuration.cs
+++ Configuration.cs
@@ -3,4 +3,5 @@
 public class Configuration
 {
     public string DeliveryFeeSerivceUrl { get; set; } = "";
+    public string DefaultConnection { get; set; } = "conn_STRING";
 }
--- Program.cs
+++ Program.cs
@@ -1,18 +1,15 @@
 using DependencyStore;
-using DependencyStore.Repositories;
-using DependencyStore.Repositories.Contracts;
-using DependencyStore.Services;
-using DependencyStore.Services.Contracts;
-using Microsoft.Data.SqlClient;
+using DependencyStore.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<Configuration>();
-builder.Services.AddScoped(x 
-    => new SqlConnection("CONN_STRING"));
-builder.Services.AddTransient<ICustomerRepository, CustomerRepository>();
-builder.Services.AddTransient<IPromoCodeRepository, PromoCodeRepository>();
-builder.Services.AddTransient<IDeliveryFeeService, DeliveryFeeService>();
+
+var connStr = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddSqlConnection(connStr);
+builder.Services.AddRepositories();
+builder.Services.AddServices();
 
 builder.Services.AddControllers();
 
