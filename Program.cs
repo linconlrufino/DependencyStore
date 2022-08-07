a85fd9ba2fefce67d69c5113cddb2b0a15e4b00c
Lincoml Rufino <linconldasilvarufino@gmail.com>
2022-08-07 20:05:52 -0300

    Resolvendo as dependências - AddSingleton

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
+using DependencyStore;
 using DependencyStore.Repositories;
 using DependencyStore.Repositories.Contracts;
 using DependencyStore.Services;
 using DependencyStore.Services.Contracts;
+using Microsoft.Data.SqlClient;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddSingleton<Configuration>();
+builder.Services.AddScoped(x 
+    => new SqlConnection("CONN_STRING"));
 builder.Services.AddTransient<ICustomerRepository, CustomerRepository>();
 builder.Services.AddTransient<IPromoCodeRepository, PromoCodeRepository>();
 builder.Services.AddTransient<IDeliveryFeeService, DeliveryFeeService>();
